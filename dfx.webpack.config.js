57b0628b9f2563f3e8865c81f0cdd39d8f66ce72
Henry Chan <henry@kinwo.net>
2022-01-17 16:56:26 +1100

    [Formatting]

--- dfx.webpack.config.js
+++ dfx.webpack.config.js
@@ -3,38 +3,32 @@ const path = require("path")
 let localCanisters, prodCanisters, canisters
 
 function initCanisterIds() {
-    try {
-        localCanisters = require(path.resolve(
-            ".dfx",
-            "local",
-            "canister_ids.json"
-        ))
-    } catch (error) {
-        console.log("No local canister_ids.json found. Continuing production")
-    }
-    try {
-        prodCanisters = require(path.resolve("canister_ids.json"))
-    } catch (error) {
-        console.log(
-            "No production canister_ids.json found. Continuing with local"
-        )
-    }
+  try {
+    localCanisters = require(path.resolve(".dfx", "local", "canister_ids.json"))
+  } catch (error) {
+    console.log("No local canister_ids.json found. Continuing production")
+  }
+  try {
+    prodCanisters = require(path.resolve("canister_ids.json"))
+  } catch (error) {
+    console.log("No production canister_ids.json found. Continuing with local")
+  }
 
-    const network =
-        process.env.DFX_NETWORK ||
-        (process.env.NODE_ENV === "production" ? "ic" : "local")
+  const network =
+    process.env.DFX_NETWORK ||
+    (process.env.NODE_ENV === "production" ? "ic" : "local")
 
-    console.info(`initCanisterIds: network=${network}`)
-    console.info(`initCanisterIds: DFX_NETWORK=${process.env.DFX_NETWORK}`)
+  console.info(`initCanisterIds: network=${network}`)
+  console.info(`initCanisterIds: DFX_NETWORK=${process.env.DFX_NETWORK}`)
 
-    canisters = network === "local" ? localCanisters : prodCanisters
+  canisters = network === "local" ? localCanisters : prodCanisters
 
-    for (const canister in canisters) {
-        process.env[`NEXT_PUBLIC_${canister.toUpperCase()}_CANISTER_ID`] =
-            canisters[canister][network]
-    }
+  for (const canister in canisters) {
+    process.env[`NEXT_PUBLIC_${canister.toUpperCase()}_CANISTER_ID`] =
+      canisters[canister][network]
+  }
 }
 
 module.exports = {
-    initCanisterIds: initCanisterIds,
+  initCanisterIds: initCanisterIds
 }
--- ui/hooks/useImageObject.js
+++ ui/hooks/useImageObject.js
@@ -32,5 +32,3 @@ export const useImageObject = imageId => {
 
   return imgSrc
 }
-
-
