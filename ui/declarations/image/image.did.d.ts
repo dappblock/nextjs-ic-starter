369e8f4152159cb5a4f9e334d5abd161bc53e064
Henry Chan <henry@kinwo.net>
2023-04-17 09:52:12 +1000

    [UI Declarations] Regenerate from dfx 0.13.1
    * cleanup unused files

--- ui/declarations/image/image.did.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Principal } from '@dfinity/principal';
-export type ImageId = string;
-export type ImageObject = Array<number>;
-export interface _SERVICE {
-  'create' : (arg_0: ImageObject) => Promise<ImageId>,
-  'delete' : (arg_0: ImageId) => Promise<undefined>,
-  'getImageById' : (arg_0: ImageId) => Promise<[] | [ImageObject]>,
-}
